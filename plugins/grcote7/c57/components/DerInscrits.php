d9cb4b003989490c6b7c07e4b5f8691f65eff92e
COTE <GrCOTE7@GMail.com>
2018-10-03 16:23:24 +0200

    CSS de Components grcote7.derInscrits interne

--- plugins/grcote7/c57/components/DerInscrits.php
+++ plugins/grcote7/c57/components/DerInscrits.php
@@ -1,6 +1,5 @@
 <?php namespace GrCOTE7\C57\Components;
 
-use ApplicationException;
 use Cms\Classes\ComponentBase;
 use RainLab\User\Models\User;
 
@@ -11,33 +10,31 @@ class DerInscrits extends ComponentBase
 
     public function componentDetails()
     {
-
         return [
             'name' => 'Derniers Inscrits',
-            'description' => 'Affiche la liste des derniers inscrits.'
+            'description' => 'Affiche la liste des derniers inscrits.',
         ];
     }
 
     public function defineProperties()
     {
-
         return [
         ];
     }
 
     public function onRun()
     {
-        //$this->derinscrits='456';
+        $this->addCss('assets/css/derInscrits.css');
         $this->derinscrits = $this->loadDerInscrits();
-//        var_dump($this->derinscrits);
+        // var_dump($this->derinscrits);
     }
 
     protected function loadDerInscrits()
     {
         $query = User::all()->sortByDesc('created_at')->take(7);
-//        echo '<pre>';
-//        var_dump($query);
-//        echo '</pre>';
+        // echo '<pre>';
+        // var_dump($query);
+        // echo '</pre>';
         return $query;
     }
 
